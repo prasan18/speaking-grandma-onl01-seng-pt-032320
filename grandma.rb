21fd73a2b1107a4bd8d3d0a154a709aec4fbaa9e
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-04 21:24:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
   if phrase== "I LOVE YOU GRANDMA!"
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase.upcase!=phrase
+  elsif phrase.upcase !=phrase
 return "HUH?! SPEAK UP, SONNY!"
 else 
-  "NO, NOT SINCE 1938!"
+  return"NO, NOT SINCE 1938!"
 end
 
-
+end
   
